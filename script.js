44a50d2e0fbbd1d9805225c7876ae3dee5ef9fad
arifvanhoulen <ariffrahmann123@gmail.com>
2024-01-12 16:23:56 +0700

    mengubah struktur overlay

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const audio = document.querySelector("audio");
 const textDisplay = document.querySelector(".text");
 const timer = document.querySelector(".timer");
 const content = document.querySelectorAll(".content");
-const btnStart = document.querySelector(".overlay-start button");
+const btnStart = document.querySelector("button.starting");
 
 content[0].style.display = "none";
 content[1].style.display = "none";
@@ -63,6 +63,34 @@ function start() {
     setTimeout(() => {
         textDisplay.textContent = "";
     }, 35000);
+    
+    setTimeout(() => {
+        textDisplay.innerHTML = `
+            wkwkwk gitu aja sih<br>
+        `;
+    
+        const btnNew = document.createElement('button');
+    
+        btnNew.innerHTML = "Lanjut respon?";
+        content[1].appendChild(btnNew);
+    
+        btnNew.addEventListener("click", function() {
+            openWhatsApp();
+        });
+    
+        function openWhatsApp() {
+            var phoneNumber = "+62895703181535";
+            var message = "Halo, saya ingin melanjutkan respon.";
+            var whatsappURL = "whatsapp://send?phone=" + phoneNumber + "&text=" + encodeURIComponent(message);
+    
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                window.location.href = whatsappURL;
+            } else {
+                alert("Maaf, browser ini tidak mendukung membuka WhatsApp secara otomatis. Silakan buka WhatsApp secara manual.");
+            }
+        }
+    }, 38000);
+    
 }
 
 btnStart.onclick = () => {
